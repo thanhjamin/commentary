26c4e263481f04983e6d995d1f660ae9c2ad4db6
Thanh Huynh <thanh@Thanhs-MacBook-Pro.local>
2015-04-30 11:47:02 -0700

    add ancestry gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,14 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def index
     @comments = Comment.all
+    @comment = Comment.new
   end
   
   def show
   end
 
   def new
-    @comment = @Comment.new
+    @comment = Comment.new
   end
 
   # POST /comments
@@ -20,7 +21,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @commentable, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -61,6 +62,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:comment)
+      params.require(:comment).permit(:comment, :full_name, :email)
     end
 end
